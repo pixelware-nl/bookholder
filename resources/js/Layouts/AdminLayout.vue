72d4c90a2415f8613411ac0ddebfe6226eee4818
Okan Ozbek <devokan@outlook.com>
2024-11-22 17:41:52 +0100

    Fix for background color

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="h-screen">
-    <nav>
-        <ul class="flex justify-end p-6 bg-slate-900 text-slate-100">
-            <NavLink slug="dashboard" route-name="dashboard.index"> Dashboard </NavLink>
-            <NavLink slug="invoices" route-name="invoices.index"> Invoicing </NavLink>
-            <li class="px-4 text-slate-500 hover:cursor-not-allowed"> Logging </li>
-            <NavLink slug="companies" route-name="companies.index"> Companies </NavLink>
-            <NavLink slug="logout" route-name="logout" method="post"> Logout </NavLink>
-        </ul>
-    </nav>
-    <main class="w-full flex justify-center pt-12 sm:bg-slate-100 xs:bg-white">
-        <div class="container px-4 w-full">
-            <slot />
-        </div>
-    </main>
+    <div class="min-h-screen sm:bg-slate-100 xs:bg-white">
+        <nav>
+            <ul class="flex justify-end p-6 bg-slate-900 text-slate-100">
+                <NavLink slug="dashboard" route-name="dashboard.index"> Dashboard </NavLink>
+                <NavLink slug="invoices" route-name="invoices.index"> Invoicing </NavLink>
+                <li class="px-4 text-slate-500 hover:cursor-not-allowed"> Logging </li>
+                <NavLink slug="companies" route-name="companies.index"> Companies </NavLink>
+                <NavLink slug="logout" route-name="logout" method="post"> Logout </NavLink>
+            </ul>
+        </nav>
+        <main class="w-full flex justify-center pt-12">
+            <div class="container px-4 w-full">
+                <slot />
+            </div>
+        </main>
     </div>
 </template>
 <script setup lang="ts">
