132f777a48b06b44228b2cb23c78d9c818a33fc7
Okan Ozbek <devokan@outlook.com>
2025-05-13 14:43:08 +0200

    Added tests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [
+            __DIR__.'/../routes/auth.php',
+            __DIR__.'/../routes/web.php',
+        ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
